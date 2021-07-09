0314b3f57b74c2de2b12157f42d10bbed05b54ea
Roger Van Wyk <roger.vanwyk@gmail.com>
2021-07-09 18:16:34 +0200

    Initial commit

---
+++ app/src/main/java/com/roger/openweather/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.roger.openweather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/*
+* Created by Roger van Wyk on Jul 9, 2021.
+* */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}.myapplication
